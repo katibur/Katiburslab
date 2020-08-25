420b69de4401f45a5864758fc3ec922fef3a7f27
user <user@Sany>
2020-08-25 13:41:14 +0600

    Added class Mul.java

--- Katiburslab/src/Mul.java
+++ Katiburslab/src/Mul.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
 
-public class Mul {
-
+public class Mul
+{
+	private static final Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args)
+	{
+		System.out.print("Enter number:");
+		int N=scanner.nextInt();
+		for(int i=1; i <= 10; i++)
+		{
+			System.out.println(N+" x "+i+" = "+N*i);
+        }
+    }
 }
